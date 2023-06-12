5f810f1f5c5f59c352c27c04f8f5d88ae32d2eef
CatMizu <zhrmghgjht@gmail.com>
2023-06-12 14:57:35 -0400

    adding docker file

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,9 @@ RUN mkdir -p /usr/src/node-app && chown -R node:node /usr/src/node-app
 
 WORKDIR /usr/src/node-app
 
+ENV PORT 3000
+ENV HOST 0.0.0.0
+
 COPY package.json yarn.lock ./
 
 USER node
@@ -13,3 +16,5 @@ RUN yarn install --pure-lockfile
 COPY --chown=node:node . .
 
 EXPOSE 3000
+
+CMD ["yarn", "dev"]
--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const logger = require('./config/logger');
 
 let server;
 mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info('Connected to MongoDB');
-  server = app.listen(config.port, () => {
+  logger.info(`Connected to MongoDB: ${config.mongoose.url}`);
+  server = app.listen(config.port, '0.0.0.0', () => {
     logger.info(`Listening to port ${config.port}`);
   });
 });
